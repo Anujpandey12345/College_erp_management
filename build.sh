1b08ee66e9ca2f162906f2bc779bcd8b1ec9566c
anujpandey12345 <your-anujpandey61687@gmail.com>
2025-07-02 10:20:57 +0530

    testeewaasdfdsfsfdg

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+pip install -r requirements.txt --no-deps
 python manage.py collectstatic --noinput
-
 # Build the project
 echo "Building the project..."
 python3.11 -m pip install -r requirements.txt
